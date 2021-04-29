628096a8ea18c22b617568f4598378fc9d54577f
MrCasades <casades@list.ru>
2021-04-29 22:49:01 +0300

    fixis

---
+++ ArrayTraining_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayTraining_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int [10];
+            int count = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                count++;
+                arr1[i] = count;
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
---
+++ array_training_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace array_training_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 3, 5, 6, 7, 0 };
+            
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+}
