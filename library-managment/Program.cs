173cbb7ae2254a7e2892fdd221d5d38c12c9b1c5
MrCasades <38098164+MrCasades@users.noreply.github.com>
2021-05-09 19:45:26 +0300

    Add library managment

--- library-managment/Program.cs
+++ library-managment/Program.cs
@@ -15,16 +15,49 @@ namespace library_managment
             while(isOpen)
             {
                 Console.WriteLine("Library");
-                Console.WriteLine("\n1 - What it is book, if i know index\n\n 2 - Find book if you know author\n\n 3 - View all books\n\n 4 - Exit");
-                Console.WriteLine("Enter number from menu");
+                Console.WriteLine("\n\n 1 - What it is book, if i know index\n\n 2 - Find book if you know author\n\n 3 - View all books\n\n 4 - Exit");
+                Console.WriteLine("\n\nEnter number from menu");
 
                 switch (Convert.ToInt32 (Console.ReadLine()))
                 {
                     case 1:
+                        int rows;
+                        int cols;
+
+                        Console.WriteLine("Enter row number");
+                        rows = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        Console.WriteLine("Enter col number");
+                        cols = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+                        Console.WriteLine("\nIt is " + books[rows, cols]);
                         break;
                     case 2:
+                        string author;
+                        bool authorIsFind = false;
+
+                        Console.WriteLine("Enter name of author: ");
+                        author = Console.ReadLine();
+                        for (int i = 0; i < books.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < books.GetLength(1); j++)
+                            {
+                                if (author.ToLower() == books[i,j].ToLower())
+                                {
+                                    Console.WriteLine("Adress of author " + author + " is " + (i + 1) + " | " + (j + 1));
+                                    authorIsFind = true;
+                                }
+                            }                          
+                        }
+
+                        if (authorIsFind == false)
+                        {
+                            Console.WriteLine("Cannot find author");
+                        }
+
                         break;
-                    case 3:
+                    case 3://Вывод всех книг
                         Console.WriteLine("All authors");
                         for (int i = 0; i < books.GetLength(0); i++)
                         {
@@ -33,15 +66,14 @@ namespace library_managment
                                 Console.Write(books[i, j] + " | ");
                             }
 
-                            Console.WriteLine();
+                            //Console.WriteLine();
                         }
                         break;
                     case 4:
                         isOpen = false;
                         break;
                 }
-
-                Console.WriteLine("Press any key to continue...");
+                Console.WriteLine("\n\nPress any key to continue...");
                 Console.ReadKey();
             }
 
